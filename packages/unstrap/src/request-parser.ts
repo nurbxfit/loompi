3888fe79fc1159f11369ca14d652288046ac2aec
nurbxfit <nurbxfit@gmail.com>
2025-11-16 20:52:17 +0800

    implement parse filters

--- packages/unstrap/src/request-parser.ts
+++ packages/unstrap/src/request-parser.ts
@@ -7,8 +7,42 @@ export interface ParsedFilters {
 export function parseFilters(query: Record<string, any>): ParsedFilters {
     const filters: ParsedFilters = {};
 
-    // todo parsing filters based on strapi spec
+    for (const [key, value] of Object.entries(query)) {
+        if (key.startsWith('filters[')) {
+            const path = key
+                .replace(/^filters/, '')
+                .replace(/\[/g, '.')
+                .replace(/\]/g, '')
+                .split('.')
+                .filter(Boolean);
 
+            let current: any = filters;
+            for (let i = 0; i < path.length - 1; i++) {
+                const segment = path[i];
+
+                // Handle array indices for $and, $or
+                if (/^\d+$/.test(segment)) {
+                    const index = parseInt(segment);
+                    const parent = path[i - 1];
+                    if (!Array.isArray(current[parent])) {
+                        current[parent] = [];
+                    }
+                    if (!current[parent][index]) {
+                        current[parent][index] = {};
+                    }
+                    current = current[parent][index];
+                } else {
+                    if (!current[segment]) {
+                        current[segment] = {};
+                    }
+                    current = current[segment];
+                }
+            }
+
+            const lastSegment = path[path.length - 1];
+            current[lastSegment] = value;
+        }
+    }
     return filters
 }
 
