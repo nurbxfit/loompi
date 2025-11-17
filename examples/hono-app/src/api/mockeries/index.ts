b24baefbb5fe48f054bc037f6c4a742bf222273e
nurbxfit <nurbxfit@gmail.com>
2025-11-17 23:34:10 +0800

    add mockery API module and change mock repository with prefix to showcase use of new createResourceRegistry function

---
+++ examples/hono-app/src/api/mockeries/index.ts
@@ -0,0 +1,73 @@
+import { mockRepoFactory } from "@/lib/repository";
+import { createRouter } from "@loompi/hono";
+import { Hono } from "hono";
+import { createFactory, createResourceRegistry, defineSchema, SchemaRegistry } from "loompi";
+
+/**
+ * Basic usage:
+ * 1. define our schemaRegistry
+ * 2. create factories
+ * 3. create resources
+ * 4. attach to main framework (eg: hono) via createRouter
+ */
+
+const schemas: SchemaRegistry = {
+    "api::mockery.mockery": defineSchema({
+        kind: "collectionType",
+        collectionName: "mockery",
+        tableName: "mockery",
+        info: {
+            singularName: "mockery",
+            pluralName: "mockeries",
+            displayName: "mockeries",
+        }
+    }),
+    "api::demo.demo": defineSchema({
+        kind: "collectionType",
+        collectionName: "demo",
+        tableName: "demo",
+        info: {
+            singularName: "demo",
+            pluralName: "demos",
+            displayName: "demos",
+        }
+    })
+}
+
+const factories = createFactory({
+    repository: mockRepoFactory,
+    schemas
+})
+
+
+const { routes, controllers } = createResourceRegistry(factories, [
+    {
+        schemaName: 'api::mockery.mockery',
+    },
+    {
+        schemaName: 'api::demo.demo',
+        controllerExtensions: ({ repository }) => ({
+            hello: async (ctx) => {
+                const data = repository("api::demo.demo").find({});
+                return ctx.res.json({
+                    data
+                })
+            }
+        }),
+        customRoutes: [
+            {
+                method: 'GET',
+                path: '/demo/hello',
+                handler: 'api::demo.demo.hello'
+            }
+        ]
+    },
+])
+
+
+const app = new Hono();
+
+const router = createRouter(app, routes, controllers);
+
+export default router;
+
--- examples/hono-app/src/index.ts
+++ examples/hono-app/src/index.ts
@@ -1,12 +1,14 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import usersModule from './api/users';
+import mockeriesModule from './api/mockeries';
 
 const app = new Hono();
 
 app.use('*', logger());
 
 app.route('/api', usersModule);
+app.route('/api', mockeriesModule)
 
 
 
