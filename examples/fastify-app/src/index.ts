849825c19607dce60506024d617b0bbe5e79afe8
nurbxfit <nurbxfit@gmail.com>
2025-11-20 18:38:54 +0800

    create example fasitfy app test loompi fastify adapter

---
+++ examples/fastify-app/src/index.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify';
+import { registerFastifyRouter } from '@loompi/fastify';
+import userRouter from './api/users'
+import fastifyRequestContext from '@fastify/request-context';
+import fastifyMultipart from '@fastify/multipart';
+
+const fastify = Fastify({ logger: true });
+
+// Register plugins
+await fastify.register(fastifyRequestContext, {
+    defaultStoreValues: {
+        user: null,
+    }
+});
+
+await fastify.register(fastifyMultipart, {
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+    }
+});
+
+// Register all routes
+registerFastifyRouter(fastify, userRouter);
+
+
+await fastify.listen({ port: 3001 });
---
+++ examples/fastify-app/src/schemas/user.ts
@@ -0,0 +1,47 @@
+import { user } from "@/db/user-schema";
+import { defineSchema } from "loompi";
+import { createInsertSchema } from 'drizzle-zod'
+import { z } from 'zod';
+
+export default defineSchema({
+    kind: 'collectionType',
+    collectionName: 'user',
+    tableName: user,
+    info: {
+        singularName: 'user',
+        pluralName: 'users',
+        displayName: 'users',
+    },
+    hooks: {
+        repository: {
+            beforeCreate: (data) => {
+                if (!data.id) {
+                    data.id = crypto.randomUUID();
+                }
+                return data;
+            },
+        },
+        controller: {
+            afterCreate: (ctx, data) => {
+                // return ctx.res.json(); // dont do this , return manipulated data instead!!
+                return { ...data, hooked: true }; // example manipulate the response data
+            },
+            afterUpdate: (ctx, data) => {
+                return { ...data, hooked: true }; // example manipulate the response data
+            }
+        }
+    },
+    validation: {
+        request: {
+            create: createInsertSchema(user, {
+                name: z.string(),
+                email: z.email(),
+            }).omit({ id: true, createdAt: true, emailVerified: true }).strict(),
+            update: createInsertSchema(user, {
+                name: z.string(),
+                email: z.email(),
+                image: z.url(),
+            }).omit({ id: true, createdAt: true, emailVerified: true }).partial().strict(), // use strict so that zod can help sanitize.
+        }
+    },
+})
